33619728da7d47e0464356bf75a7eb111146e95e
melissafanea01 <101254368+melissafanea01@users.noreply.github.com>
2023-02-01 13:43:18 +0200

    unnindstal the app react

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import{Navbar} from "react-boostrap";
+
+
+
+
+
+
+export const Navbar =() => {
+    return(
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    
+                </Navbar.Brand>
+            </Navbar.Header>
+        </Navbar>
+
+    )
+}
